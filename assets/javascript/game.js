448de6047e46f6f8bf3b46d26dedfef8840e1ca7
anaswardeh <wardeh@un.org>
2017-09-16 20:02:34 -0400

    New Files

---
+++ assets/javascript/game.js
@@ -0,0 +1,197 @@
+$(document).ready(function() {
+
+
+    var random = 0;
+
+    //Creating four different variables with a value of 0
+
+    var red = 0;
+    var blue = 0;
+    var yellow = 0;
+    var green = 0;
+
+    var winnings = 0;
+    var losses = 0;
+
+
+
+    var total = red + blue + yellow + green;
+
+    //On loading the page, a random number between 1 and 50 should appear on the third box.
+
+
+
+    $("#guess").html(random = Math.floor((Math.random() * 75) + 25));
+    $("#score").html(total);
+
+
+    //printResults();
+
+    //Each variable is assigned to a different image
+
+    $("#red").one("click", function() {
+        red = Math.floor((Math.random() * 20) + 1);
+        total += red;
+        $("#score").html(total);
+
+
+
+
+        if (total === random) {
+            winnings++;
+            printResults();
+        } else if (total > random) {
+            losses++
+            printResults();
+        }
+
+
+
+        $("#red").on("click", function() {
+            total += red;
+            $("#score").html(total);
+
+
+
+
+            if (total === random) {
+                winnings++;
+                printResults();
+            } else if (total > random) {
+                losses++
+                printResults();
+            }
+
+        });
+
+
+    });
+
+
+    $("#blue").one("click", function() {
+        blue = Math.floor((Math.random() * 20) + 1);
+        total += blue;
+        $("#score").html(total);
+
+
+        if (total === random) {
+            winnings++;
+            printResults();
+        } else if (total > random) {
+            losses++
+            printResults();
+        }
+
+
+
+
+        $("#blue").on("click", function() {
+            total += blue;
+            $("#score").html(total);
+
+
+            if (total === random) {
+                winnings++;
+                printResults();
+            } else if (total > random) {
+                losses++
+                printResults();
+            }
+
+
+        });
+
+
+
+    });
+
+
+    $("#yellow").one("click", function() {
+        yellow = Math.floor((Math.random() * 20) + 1);
+        total += yellow;
+        $("#score").html(total);
+
+        if (total === random) {
+            winnings++;
+            printResults();
+        } else if (total > random) {
+            losses++
+            printResults();
+        }
+
+
+
+        $("#yellow").on("click", function() {
+            total += yellow;
+            $("#score").html(total);
+
+
+            if (total === random) {
+                winnings++;
+                printResults();
+            } else if (total > random) {
+                losses++
+                printResults();
+            }
+
+        });
+
+
+    });
+
+    $("#green").one("click", function() {
+        green = Math.floor((Math.random() * 20) + 1);
+        total += green;
+        $("#score").html(total);
+
+        if (total === random) {
+            winnings++;
+            printResults();
+        } else if (total > random) {
+            losses++
+            printResults();
+        }
+
+
+        $("#green").on("click", function() {
+            total += green;
+            $("#score").html(total);
+
+
+            if (total === random) {
+                winnings++;
+                printResults();
+            } else if (total > random) {
+                losses++
+                printResults();
+            }
+
+
+        });
+
+
+
+
+    });
+
+    //Reset and print value
+
+    function printResults() {
+
+        $("#results").html("<p> Wins: " + winnings + "</p>" +
+            " <p> losses: " + losses + "</p>");
+
+        random = Math.floor((Math.random() * 75) + 25);
+        red = Math.floor((Math.random() * 20));
+        blue = Math.floor((Math.random() * 20));
+        yellow = Math.floor((Math.random() * 20));
+        green = Math.floor((Math.random() * 20));
+        total = 0;
+        $("#guess").html(random);
+        $("#score").html(total);
+
+
+    };
+
+
+});
---
+++ assets/javascript/game1.js
@@ -0,0 +1,113 @@
+$(document).ready(function() {
+
+    var currentScore = 0;
+    var winnings = 0;
+    var losses = 0;
+
+    var rand = Math.floor((Math.random() * 75) + 25);
+
+    $("#guess").html(rand);
+
+    var crystal = {
+
+        red: {
+            value: 0
+        },
+
+
+        blue: {
+            value: 0
+        },
+
+
+        yellow: {
+            value: 0
+        },
+
+
+        green: {
+            value: 0
+        }
+    }
+
+    crystal.red.value = Math.floor((Math.random() * 20) + 1);
+    crystal.blue.value = Math.floor((Math.random() * 20) + 1);
+    crystal.yellow.value = Math.floor((Math.random() * 20) + 1);
+    crystal.green.value = Math.floor((Math.random() * 20) + 1);
+
+
+    var addValues = function(clickCyrstal) {
+        currentScore += clickCyrstal.value;
+    }
+
+    $("#red").on("click", function() {
+        addValues(crystal.red);
+        $("#score").html(currentScore);
+
+        if (currentScore === rand) {
+            winnings++;
+            reset();
+
+        } else if (currentScore > rand) {
+            losses++;
+            reset();
+        }
+    })
+    $("#blue").on("click", function() {
+        addValues(crystal.blue);
+        $("#score").html(currentScore);
+        if (currentScore === rand) {
+            winnings++;
+            reset();
+
+        } else if (currentScore > rand) {
+            losses++;
+            reset();
+        }
+
+    })
+    $("#yellow").on("click", function() {
+        addValues(crystal.yellow);
+        $("#score").html(currentScore);
+        if (currentScore === rand) {
+            winnings++;
+            reset();
+
+        } else if (currentScore > rand) {
+            losses++;
+            reset();
+        }
+
+    })
+    $("#green").on("click", function() {
+        addValues(crystal.green);
+        $("#score").html(currentScore);
+        if (currentScore === rand) {
+            winnings++;
+            reset();
+
+        } else if (currentScore > rand) {
+            losses++;
+            reset();
+        }
+    })
+
+
+    function reset() {
+        $("#results").html("<p> Wins: " + winnings + "</p>" +
+            " <p> losses: " + losses + "</p>");
+        rand = Math.floor((Math.random() * 75) + 25);
+        currentScore = 0;
+        crystal.red.value = Math.floor((Math.random() * 20));
+        crystal.blue.value = Math.floor((Math.random() * 20));
+        crystal.yellow.value = Math.floor((Math.random() * 20));
+        crystal.green.value = Math.floor((Math.random() * 20));
+        $("#guess").html(rand);
+        $("#score").html(currentScore);
+
+    }
+
+
+
+
+});
